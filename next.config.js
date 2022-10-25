780b2072fe89554fbf668466f64665ef8191cf74
Exzo Network <admin@exzocoin.com>
2022-10-25 17:01:54 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['saisi-nft-marketplace.infura-ipfs.io'],
+    protocol: 'https',
+    hostname: 'exzo-network.infura-ipfs.io',
+    port: '',
+    pathname: '/ipfs/**',
+    domains: ['exzo-network.infura-ipfs.io'],
   },
 };
 
